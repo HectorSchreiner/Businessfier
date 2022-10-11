c19b7980121ab55fa619f35ea25dfcdb6bae064a
HectorSchreiner <agenthector007@gmail.com>
2022-10-11 14:04:37 +0200

    parser bist ready
    yes

--- ParseFile.java
+++ ParseFile.java
@@ -31,15 +31,21 @@ public class ParseFile {
     public String readList(ArrayList<String> inputList) {
         String listString = inputList.stream().map(Object::toString)
                 .collect(Collectors.joining(" "));
+        replacer(listString);
+
         return listString;
     }
 
+    private String replacer(String input) {
+        input = input.replace(" ,", ",");
+        input = input.replace(" .", ".");
+        input = input.replace(" ,", ",");
+        input = input.replace(" ,", ",");
+        return input;
+    }
+
     private ArrayList<String> splitStringToList(String input) {
-        input = input.replace(",", " ,");
-        input = input.replace(".", " .");
-        input = input.replace("!", " !");
-        input = input.replace(":", " :");
-        input = input.replace("-", " - ");
+        replacer(input);
 
         List<String> arr = new ArrayList<String>(Arrays.asList(input.split(" ")));
         return (ArrayList<String>) arr;
--- Utils.java
+++ Utils.java
@@ -1,7 +1,3 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class Utils {
 
