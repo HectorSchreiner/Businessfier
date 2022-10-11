a09b3aeb106bf308d3d711cc7031f653e181ecaf
HectorSchreiner <hectorschousbo@gmail.com>
2022-10-11 19:49:38 +0200

    idk fixed something

--- WordCombiner.java
+++ WordCombiner.java
@@ -1,26 +1,27 @@
 import java.util.ArrayList;
-import java.lang.Math;  
+import java.io.IOException;
+import java.lang.Math;
 
 public class WordCombiner {
     ArrayList<String> businessWords;
     ArrayList<Boolean> placement;
     ArrayList<String> Words;
 
-    public WordCombiner(ArrayList<String> input) {
-        this.Words = input;
+    public WordCombiner(String fileName) throws IOException {
+        ParseFile parser = new ParseFile();
+        parser.readFile(fileName);
+        this.Words = parser.FileArray;
         GetPlacement();
 
         // gets business words from businessWords clas
-        BusinessWords a = new BusinessWords();
-        a.GetWords();
-        this.businessWords = a.BWords;
+        BusinessWords b = new BusinessWords();
+        b.GetWords();
+        this.businessWords = b.businessWordArray;
     }
 
     public void GetPlacement() {
         WordChecker checker = new WordChecker(this.Words);
-
         checker.CheckLength();
-
         this.placement = checker.needsWord;
     }
 
@@ -29,18 +30,17 @@ public class WordCombiner {
 
         for (int i = 0; i < this.placement.size(); i++) {
             System.out.println(this.placement.get(i));
-            if(this.placement.get(i)){
-                
-                
-                int randBusinessIndex = (int) Math.floor((this.businessWords.size()*Math.random()));
 
-                this.Words.add(i+counter, businessWords.get(randBusinessIndex));
+            if (this.placement.get(i)) {
+                int randBusinessIndex = (int) Math.floor((this.businessWords.size() * Math.random()));
+                System.out.println(randBusinessIndex);
+                this.Words.add(i + counter, businessWords.get(randBusinessIndex));
                 counter++;
             }
         }
 
         for (var i : this.Words) {
-            System.out.print(i+" ");
+            System.out.print(i + " ");
         }
     }
 }
